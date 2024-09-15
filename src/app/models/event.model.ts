a84a2da98d11b796448a90a818b63c926c2d5a4d
gmiki91 <gmiki91@gmail.com>
2024-09-15 14:43:32 +0200

    participants length, list overflow

--- src/app/models/event.model.ts
+++ src/app/models/event.model.ts
@@ -1,9 +1,11 @@
+import User from "./user.model";
+
 export type Event={
     id:string,
     title:string,
     location:string,
     time:string,
-    joined:string[];
+    participants:User[];
     min:number;
     max?:number;
     info:string;
