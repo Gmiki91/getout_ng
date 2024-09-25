8f1090c86b381e7301e5d45b85bd64ea10a8dc0d
Miklos Gabor <gmiki91@gmail.com>
2024-09-25 15:32:19 +0200

    extended timePipe

--- src/app/event-list/event/event.component.ts
+++ src/app/event-list/event/event.component.ts
@@ -1,6 +1,6 @@
 import { Component,input,inject} from '@angular/core';
 import { Event } from '../../models/event.model';
-import { TimeUntilPipe } from '../../pipes/time-until.pipe';
+import { TimeTextPipe } from '../../pipes/time-until.pipe';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import { DistanceFormatPipe } from '../../pipes/distance-format.pipe';
@@ -9,7 +9,7 @@ import { MapService } from '../../services/map.service';
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [TimeUntilPipe,MatCardModule,MatIconModule,DistanceFormatPipe],
+  imports: [TimeTextPipe,MatCardModule,MatIconModule,DistanceFormatPipe],
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss',
 })
--- src/app/pipes/time-until.pipe.ts
+++ src/app/pipes/time-until.pipe.ts
@@ -1,32 +1,35 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-    name: 'timeUntil',
+    name: 'timeText',
     standalone: true,
 })
-export class TimeUntilPipe implements PipeTransform {
-    transform(value: string): string {
+export class TimeTextPipe implements PipeTransform {
+    transform(value: string, format:'until' | 'since'): string {
         const eventDate = new Date(value);
         const now = new Date();
-        const millisecondsDiff = eventDate.getTime() - now.getTime();
+        const millisecondsDiff = format === 'until' ? eventDate.getTime() - now.getTime() : now.getTime()-eventDate.getTime();
 
+        //only applicable to until format
         if (millisecondsDiff < 0) {
             return 'Started at ' + this.formatTime(eventDate);
         }
+
+        const appendix = format === 'until' ? 'left' : 'ago';
 
         const minutesDiff = Math.round(millisecondsDiff / (1000 * 60));
         const hoursDiff = Math.floor(minutesDiff / 60);
         const daysDiff = Math.floor(hoursDiff / 24);
 
         if (daysDiff >= 1) {
-            return daysDiff === 1 ? '1 day left' : `${daysDiff} days left`;
+            return daysDiff === 1 ? `1 day ${appendix}` : `${daysDiff} days ${appendix}`;
         }
 
         if (hoursDiff >= 1) {
-            return hoursDiff === 1 ? '1 hour left' : `${hoursDiff} hours left`;
+            return hoursDiff === 1 ? `1 hour ${appendix}` : `${hoursDiff} hours ${appendix}`;
         }
 
-        return minutesDiff === 1 ? '1 minute left' : `${minutesDiff} minutes left`;
+        return minutesDiff === 1 ? `1 minute ${appendix}` : `${minutesDiff} minutes ${appendix}`;
     }
 
     private formatTime(date: Date): string {
