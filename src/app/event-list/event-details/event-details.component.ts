9656f8bdf7d064187eb028fee49be8df904165ea
Miklos Gabor <gmiki91@gmail.com>
2024-09-25 15:58:12 +0200

    custom scrollbar

--- src/app/event-list/event-details/event-details.component.ts
+++ src/app/event-list/event-details/event-details.component.ts
@@ -1,63 +1,84 @@
-import { Component,ElementRef,inject,input,output, ViewChild,OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  input,
+  output,
+  ViewChild,
+  OnInit,
+} from '@angular/core';
 import { Event } from '../../models/event.model';
 import { DatePipe } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 import { KommentService } from '../../services/comment.service';
 import { NewKommentData } from '../../models/komment.model';
 import { TextFieldModule } from '@angular/cdk/text-field';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatListModule} from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
 import { TimeTextPipe } from '../../pipes/time-until.pipe';
+import { CustomScrollDirective } from '../../custom-scroll.directive';
 
 @Component({
   selector: 'app-event-details',
   standalone: true,
-  imports: [DatePipe,MatButton,TextFieldModule,MatCardModule,MatDividerModule,MatFormFieldModule,MatListModule,MatInputModule,TimeTextPipe],
+  imports: [
+    DatePipe,
+    MatButton,
+    TextFieldModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatInputModule,
+    TimeTextPipe,
+    CustomScrollDirective,
+  ],
   templateUrl: './event-details.component.html',
-  styleUrl: './event-details.component.scss'
+  styleUrl: './event-details.component.scss',
 })
-
 export class EventDetailsComponent implements OnInit {
   @ViewChild('komment')
   kommentRef!: ElementRef;
-  event=input.required<Event>();
-  joined=input.required<boolean>();
+  event = input.required<Event>();
+  joined = input.required<boolean>();
   participation = output<boolean>();
   deleteEvent = output();
   closeDialog = output();
-  kommentService = inject(KommentService)
+  kommentService = inject(KommentService);
   komments = this.kommentService.comments;
-  userId = "";
+  userId = '';
 
   ngOnInit(): void {
-    this.userId=localStorage.getItem('uuid')!;
-      this.kommentService.getKomments(this.event().id);
-  }
-  onJoin():void{
-    this.participation.emit(true);
+    this.userId = localStorage.getItem('uuid')!;
+    this.kommentService.getKomments(this.event().id);
   }
   
-  onLeave():void{
+  onJoin(): void {
+    this.participation.emit(true);
+  }
+
+  onLeave(): void {
     this.participation.emit(false);
   }
 
-  onDelete():void{
+  onDelete(): void {
     this.deleteEvent.emit();
   }
 
-  onCloseDialog():void{
+  onCloseDialog(): void {
     this.closeDialog.emit();
   }
-  onAddKomment():void{
-    const komment:NewKommentData = {
-      text:this.kommentRef.nativeElement.value,
-      timestamp:new Date().toISOString(),
-      eventId:this.event().id,
-      userId:this.userId
-    }
-     this.kommentService.addKomment(komment);
+
+  onAddKomment(): void {
+    const komment: NewKommentData = {
+      text: this.kommentRef.nativeElement.value,
+      timestamp: new Date().toISOString(),
+      eventId: this.event().id,
+      userId: this.userId,
+    };
+    this.kommentService.addKomment(komment);
   }
 }
