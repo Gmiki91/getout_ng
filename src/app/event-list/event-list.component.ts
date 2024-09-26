8702f658e4bb0a4580451d7f72f67a18a3a708f5
Miklos Gabor <gmiki91@gmail.com>
2024-09-26 22:49:40 +0200

    design event list

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -2,12 +2,11 @@ import { Component, input, output } from '@angular/core';
 import { Event } from '../models/event.model';
 import { EventComponent } from './event/event.component';
 import { EventDetailsComponent } from './event-details/event-details.component';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
-  imports: [EventComponent, EventDetailsComponent, MatButtonModule],
+  imports: [EventComponent, EventDetailsComponent],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.scss',
 })
