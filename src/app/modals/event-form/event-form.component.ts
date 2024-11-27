d8550ba3c07387c762c2abe9bf24380b1ff34d1c
Miklos Gabor <gmiki91@gmail.com>
2024-11-27 15:19:48 +0100

    add recurring events

--- src/app/modals/event-form/event-form.component.ts
+++ src/app/modals/event-form/event-form.component.ts
@@ -9,17 +9,20 @@ import {
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatButton } from '@angular/material/button';
 import { Subject, takeUntil } from 'rxjs';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { EventsService } from '../../services/events.service';
 import { MapService } from '../../services/map.service';
-import {TIMES} from '../../time'
+import { TIMES } from '../../time';
 import { MatCardModule } from '@angular/material/card';
 import { slideDown } from '../../utils/utils';
 import { MatIcon } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { Recurrence } from '../../models/event.model';
+
 @Component({
   selector: 'app-event-form',
   standalone: true,
@@ -34,11 +37,12 @@ import { MatIcon } from '@angular/material/icon';
     MatDatepickerModule,
     MatSelectModule,
     MatCardModule,
-    MatIcon
+    MatIcon,
+    MatRadioModule,
   ],
   templateUrl: './event-form.component.html',
   styleUrl: './event-form.component.scss',
-  animations:[slideDown]
+  animations: [slideDown],
 })
 export class EventFormComponent implements OnInit, OnDestroy {
   @ViewChild('locationInput')
@@ -46,13 +50,14 @@ export class EventFormComponent implements OnInit, OnDestroy {
   locationSelectMode = false;
   minPeople = 2;
   times = TIMES;
-  endTimes:string[] = [];
-  selectedTime='';
-  selectedEndTime='';
-  isEndTime=false;
+  endTimes: string[] = [];
+  selectedTime = '';
+  selectedEndTime = '';
+  isEndTime = false;
   minDate = new Date(); // min date is today
-  startDate=new Date();
-  endingDate=new Date();
+  startDate = new Date();
+  endingDate = new Date();
+  durationInDays = 0;
   unsubscribe$ = new Subject<void>();
   private eventsService: EventsService = inject(EventsService);
   private mapService: MapService = inject(MapService);
@@ -60,15 +65,15 @@ export class EventFormComponent implements OnInit, OnDestroy {
   markerPosition = this.mapService.markerPosition; // stores address selected from the map
 
   //Set up the default selected time ~ 30 minutes ahead of now
-  ngOnInit():void{
+  ngOnInit(): void {
     const hour = new Date().getHours();
-    if(hour === 23){
-      this.minDate.setDate(new Date().getDate()+1) //tomorrow
+    if (hour === 23) {
+      this.minDate.setDate(new Date().getDate() + 1); //tomorrow
       this.selectedTime = TIMES[0];
       this.selectedEndTime = TIMES[1];
-    }else{
-      this.selectedTime = hour ===0 ? TIMES[1] : TIMES[hour*2+1];
-      this.selectedEndTime = hour ===0 ? TIMES[2] : TIMES[hour*2+2];
+    } else {
+      this.selectedTime = hour === 0 ? TIMES[1] : TIMES[hour * 2 + 1];
+      this.selectedEndTime = hour === 0 ? TIMES[2] : TIMES[hour * 2 + 2];
     }
     this.checkEndTimeHours();
   }
@@ -82,40 +87,64 @@ export class EventFormComponent implements OnInit, OnDestroy {
     this.eventsService.toggleEventForm();
   }
 
-  toogleEndTime(){
+  toogleEndTime() {
     this.isEndTime = !this.isEndTime;
   }
 
-  checkEndTimeHours(){
+  checkEndTimeHours() {
     if (this.endingDate?.toDateString() === this.startDate?.toDateString()) {
-      this.endTimes = this.times.filter(time => time > this.selectedTime);
+      this.endTimes = this.times.filter((time) => time > this.selectedTime);
       this.selectedEndTime = this.endTimes[0] || '00:00';
     } else {
       this.endTimes = [...this.times];
     }
-    
+    this.updateDuration();
   }
-  toggleLocationSelect(){
+
+  updateDuration() {
+    // endingDate is set to startDate, but if we change startDate, it is possible the endDate is earlier
+    // (onSubmit gets the endDate from the form, so its fine)
+    // if thats the case, endDate was not initilaized, so duration is still less than 1 day
+    if (this.startDate.getTime() >= this.endingDate.getTime()) {
+      this.durationInDays = 0;
+    } else {
+      const start = new Date(
+        this.initTime(this.startDate, this.selectedTime)
+      ).getTime();
+      const end = new Date(
+        this.initTime(this.endingDate, this.selectedEndTime)
+      ).getTime();
+      const diffInMs = end - start;
+      this.durationInDays = diffInMs / 1000 / 60 / 60 / 24;
+    }
+  }
+
+  toggleLocationSelect() {
     this.locationSelectMode = !this.locationSelectMode;
   }
   onSubmit(form: NgForm) {
-    if (form.valid && (this.markerAddress() && this.markerPosition())) {
-      const { title, date, endDate, min, max, info } = form.form.value;
-      const time = this.initTime(date,this.selectedTime);
-      const endTime = endDate ? this.initTime(endDate,this.selectedEndTime) : undefined;
+    if (form.valid && this.markerAddress() && this.markerPosition()) {
+      const { title, date, endDate, min, max, info, recurring } =
+        form.form.value;
+      const time = this.initTime(date, this.selectedTime);
+      const endTime = endDate
+        ? this.initTime(endDate, this.selectedEndTime)
+        : undefined;
       const location = this.markerAddress();
       const latLng = this.markerPosition();
       this.mapService.removeTemporaryMarker();
+      let finalRecurring = this.checkDuration(this.durationInDays, recurring);
       this.eventsService
         .addEvent({
           title: title,
-          location: location, //correct address from map click
-          latLng:  latLng,
+          location: location,
+          latLng: latLng,
           time: time,
-          endTime:endTime || time,
+          endTime: endTime || time,
           min: min,
           max: max,
           info: info,
+          recurring: finalRecurring
         })
         .pipe(takeUntil(this.unsubscribe$))
         .subscribe({
@@ -127,19 +156,30 @@ export class EventFormComponent implements OnInit, OnDestroy {
             console.error('Error adding event:', error);
           },
         });
-    }else if(!form.valid){
-      alert("Form is invalid")
-    }else if(this.mapService.markerAddress()==""){
-      alert("Select an address from the map");
+    } else if (!form.valid) {
+      alert('Form is invalid');
+    } else if (this.mapService.markerAddress() == '') {
+      alert('Select an address from the map');
     }
   }
-  initTime(date:Date,time:string){
+  private initTime(date: Date, time: string) {
     const result = new Date(date);
-      const hour = +time.substring(0,2);
-      const minute = +time.substring(3);
-      result.setHours(hour);
-      result.setMinutes(minute)
-      result.setSeconds(0);
-      return result.toISOString();
+    const hour = +time.substring(0, 2);
+    const minute = +time.substring(3);
+    result.setHours(hour);
+    result.setMinutes(minute);
+    result.setSeconds(0);
+    return result.toISOString();
+  }
+
+  private checkDuration(durationInDays: number, recurring: Recurrence): Recurrence {
+    if (
+      (durationInDays > 1 && recurring == 'daily') ||
+      (durationInDays > 7 && recurring == 'weekly') ||
+      (durationInDays > 28 && recurring == 'monthly')
+    ) {
+      return 'never';
+    }
+    return recurring;
   }
 }
--- src/app/models/event.model.ts
+++ src/app/models/event.model.ts
@@ -1,33 +1,36 @@
-import { Komment } from "./komment.model";
-import User from "./user.model";
-export type LatLng={
-    lat:number,
-    lng:number
-}
-export type Event={
-    id:string,
-    title:string,
-    location:string,
-    latLng:LatLng,
-    distance:number,
-    time:string,
-    endTime:string,
-    participants:User[];
-    min:number;
-    max:number;
-    info:string;
-    ownerId:string;
-    komments:Komment[];
-}
+import { Komment } from './komment.model';
+import User from './user.model';
+export type Recurrence = 'never' | 'daily' | 'weekly' | 'monthly';
+export type LatLng = {
+  lat: number;
+  lng: number;
+};
+export type Event = {
+  id: string;
+  title: string;
+  location: string;
+  latLng: LatLng;
+  distance: number;
+  time: string;
+  endTime: string;
+  participants: User[];
+  min: number;
+  max: number;
+  info: string;
+  ownerId: string;
+  komments: Komment[];
+  recurring: Recurrence
+};
 
-export type NewEventData={
-    title:string,
-    location:string,
-    latLng:{lat:number,lng:number},
-    time:string,
-    endTime:string,
-    min:number;
-    max?:number;
-    info?:string;
-    ownerId?:string
-}
+export type NewEventData = {
+  title: string;
+  location: string;
+  latLng: { lat: number; lng: number };
+  time: string;
+  endTime: string;
+  min: number;
+  max?: number;
+  info?: string;
+  ownerId?: string;
+  recurring: Recurrence
+};
