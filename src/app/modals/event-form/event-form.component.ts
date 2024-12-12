abace3535f31b3ec7516b222d4e6920d47d544f0
Miklos Gabor <gmiki91@gmail.com>
2024-12-12 20:13:42 +0100

    extract time field

--- src/app/modals/event-form/event-form.component.ts
+++ src/app/modals/event-form/event-form.component.ts
@@ -2,18 +2,14 @@ import {
   Component,
   DestroyRef,
   inject,
-  OnInit
+  ViewChild,
 } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatButton,MatIconButton } from '@angular/material/button';
-import { provideNativeDateAdapter } from '@angular/material/core';
 import { EventsService } from '../../services/events.service';
 import { MapService } from '../../services/map.service';
-import { TIMES } from '../../time';
 import { MatCardModule } from '@angular/material/card';
 import { slideDown } from '../../utils/utils';
 import { MatIcon } from '@angular/material/icon';
@@ -21,12 +17,12 @@ import { MatRadioModule } from '@angular/material/radio';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { Recurrence } from '../../models/event.model';
 import { LocationInfoComponent } from "../location-info/location-info.component";
-import { LocationField } from "../form-fields/location-field.component";
+import { LocationFieldComponent } from "../form-fields/location-field.component";
+import { TimeFieldComponent } from '../form-fields/time-field/time-field.component';
 
 @Component({
   selector: 'app-event-form',
   standalone: true,
-  providers: [provideNativeDateAdapter()],
   imports: [
     FormsModule,
     MatFormField,
@@ -35,32 +31,24 @@ import { LocationField } from "../form-fields/location-field.component";
     MatIconButton,
     MatLabel,
     MatError,
-    MatDatepickerModule,
-    MatSelectModule,
     MatCardModule,
     MatIcon,
     MatRadioModule,
     MatCheckboxModule,
     LocationInfoComponent,
-    LocationField
+    LocationFieldComponent,
+    TimeFieldComponent
 ],
   templateUrl: './event-form.component.html',
   styleUrl: '../event-form.component.scss',
   animations: [slideDown],
 })
-export class EventFormComponent implements OnInit {
+export class EventFormComponent{
+  @ViewChild(TimeFieldComponent)
+  timeField! :TimeFieldComponent;
   locationSelectMode = false;
   showOSMInfo = false;
   minPeople = 2;
-  times = TIMES;
-  endTimes: string[] = [];
-  selectedTime = '';
-  selectedEndTime = '';
-  isEndTime = false;
-  minDate = new Date(); // min date is today
-  startDate = new Date();
-  endingDate = new Date();
-  durationInDays = 0;
   repeats =false
   destroyRef = inject(DestroyRef);
   eventsService: EventsService = inject(EventsService);
@@ -68,58 +56,13 @@ export class EventFormComponent implements OnInit {
   markerAddress = this.mapService.markerAddress; // stores address selected from the map
   markerPosition = this.mapService.markerPosition; // stores address selected from the map
 
-  //Set up the default selected time ~ 30 minutes ahead of now
-  ngOnInit(): void {
-    const hour = new Date().getHours();
-    if (hour === 23) {
-      this.minDate.setDate(new Date().getDate() + 1); //tomorrow
-      this.selectedTime = TIMES[0];
-      this.selectedEndTime = TIMES[1];
-    } else {
-      this.selectedTime = hour === 0 ? TIMES[1] : TIMES[hour * 2 + 1];
-      this.selectedEndTime = hour === 0 ? TIMES[2] : TIMES[hour * 2 + 2];
-    }
-    this.checkEndTimeHours();
-  }
 
   onClose() {
     this.locationSelectMode ? this.locationSelectMode=false : this.eventsService.toggleEventForm();
   }
 
-  toogleEndTime() {
-    this.isEndTime = !this.isEndTime;
-  }
-
   toggleOSMInfo(){
     this.showOSMInfo=!this.showOSMInfo;
-  }
-
-  checkEndTimeHours() {
-    if (this.endingDate?.toDateString() === this.startDate?.toDateString()) {
-      this.endTimes = this.times.filter((time) => time > this.selectedTime);
-      this.selectedEndTime = this.endTimes[0] || '00:00';
-    } else {
-      this.endTimes = [...this.times];
-    }
-    this.updateDuration();
-  }
-
-  updateDuration() {
-    // endingDate is set to startDate, but if we change startDate, it is possible the endDate is earlier
-    // (onSubmit gets the endDate from the form, so its fine)
-    // if thats the case, endDate was not initilaized, so duration is still less than 1 day
-    if (this.startDate.getTime() >= this.endingDate.getTime()) {
-      this.durationInDays = 0;
-    } else {
-      const start = new Date(
-        this.initTime(this.startDate, this.selectedTime)
-      ).getTime();
-      const end = new Date(
-        this.initTime(this.endingDate, this.selectedEndTime)
-      ).getTime();
-      const diffInMs = end - start;
-      this.durationInDays = diffInMs / 1000 / 60 / 60 / 24;
-    }
   }
 
   toggleLocationSelect() {
@@ -127,16 +70,13 @@ export class EventFormComponent implements OnInit {
   }
   onSubmit(form: NgForm) {
     if (form.valid && this.markerAddress() && this.markerPosition()) {
-      const { title, date, endDate, min, max, info, recurring } =
-        form.form.value;
-      const time = this.initTime(date, this.selectedTime);
-      const endTime = endDate
-        ? this.initTime(endDate, this.selectedEndTime)
-        : undefined;
+      const { title,min, max, info, recurring } = form.form.value;
+      const time = this.timeField.getStartTime();
+      const endTime = this.timeField.isEndTime ? this.timeField.getEndTime() : undefined;
       const location = this.markerAddress();
       const latLng = this.markerPosition();
       this.mapService.removeTemporaryMarker();
-      let finalRecurring = this.checkDuration(this.durationInDays, recurring);
+      let finalRecurring = this.checkDuration(this.timeField.durationInDays, recurring);
       const sub = this.eventsService
         .addEvent({
           title: title,
@@ -165,15 +105,7 @@ export class EventFormComponent implements OnInit {
       alert('Select an address from the map');
     }
   }
-  private initTime(date: Date, time: string) {
-    const result = new Date(date);
-    const hour = +time.substring(0, 2);
-    const minute = +time.substring(3);
-    result.setHours(hour);
-    result.setMinutes(minute);
-    result.setSeconds(0);
-    return result.toISOString();
-  }
+
 
   private checkDuration(durationInDays: number, recurring: Recurrence): Recurrence {
     if (
---
+++ src/app/modals/form-fields/time-field/time-field.component.ts
@@ -0,0 +1,119 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TIMES } from '../../../time';
+import { MatFormField,  } from '@angular/material/form-field';
+import {  MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { provideNativeDateAdapter } from '@angular/material/core';
+@Component({
+  selector: 'app-time-field',
+  templateUrl: './time-field.component.html',
+  styleUrl:'../../event-form.component.scss',
+  providers: [provideNativeDateAdapter()],
+  standalone:true,
+  imports:[ReactiveFormsModule,MatFormField,MatDatepickerModule,MatInputModule,MatSelectModule]
+})
+export class TimeFieldComponent implements OnInit {
+  form!: FormGroup;
+  times = TIMES;
+  endTimes: string[] = [];
+  isEndTime = false;
+  minDate = new Date(); // Min date is today
+  durationInDays = 0;
+  fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.buildForm();
+    this.subscribeToStartDateChange();
+    this.checkEndTimeHours();
+  }
+
+  buildForm():void{
+    const currentHour = new Date().getHours();
+    const defaultStartTime =
+      currentHour === 0 ? TIMES[1] : TIMES[currentHour * 2 + 2];
+    const defaultEndTime =
+      currentHour === 0 ? TIMES[2] : TIMES[currentHour * 2 + 2];
+
+      this.form = this.fb.group({
+        startDate: [new Date(), Validators.required],
+        selectedTime: [defaultStartTime, Validators.required],
+        endDate: [new Date()],
+        selectedEndTime: [defaultEndTime, ],
+      });
+  }
+
+  subscribeToStartDateChange():void{
+    this.form.get('startDate')?.valueChanges.subscribe((startDate) => {
+        const endDateControl = this.form.get('endDate');
+        endDateControl?.setValue(startDate)
+        
+      });
+  }
+
+  toogleEndTime() {
+    this.isEndTime = !this.isEndTime;
+      }
+
+  checkEndTimeHours() {
+    const startDate = this.form.get('startDate')?.value;
+    const selectedTime = this.form.get('selectedTime')?.value;
+
+    if (this.form.get('endDate')?.value?.toDateString() === startDate?.toDateString()) {
+        this.endTimes = this.times.filter((time) => time > selectedTime);
+      this.form.patchValue({ selectedEndTime: this.endTimes[0] || '00:00' });
+    } else {
+      this.endTimes = [...this.times];
+    }
+
+    this.updateDuration();
+  }
+
+   /**
+    * endingDate is set to startDate, but if we change startDate, it is possible the endDate is earlier
+    * (onSubmit gets the endDate from the form, so its fine)
+    * if thats the case, endDate was not initilaized, so duration is still less than 1 day
+    */
+  updateDuration() {
+    const startDate = this.form.get('startDate')?.value;
+    const endDate = this.form.get('endDate')?.value;
+    const selectedTime = this.form.get('selectedTime')?.value;
+    const selectedEndTime = this.form.get('selectedEndTime')?.value;
+
+       if (startDate.getTime() >= endDate.getTime()) {
+         this.durationInDays = 0;
+       } else {
+         const start = new Date(
+           this.initTime(startDate, selectedTime)
+         ).getTime();
+         const end = new Date(
+           this.initTime(endDate, selectedEndTime)
+         ).getTime();
+         const diffInMs = end - start;
+         this.durationInDays = diffInMs / 1000 / 60 / 60 / 24;
+       }
+  }
+
+  getStartTime():string{
+    const date = this.form.get('startDate')?.value;
+    const time = this.form.get('selectedTime')?.value;
+    return  this.initTime(date, time);
+  }
+
+  getEndTime():string{
+    const date = this.form.get('endDate')?.value;
+    const time = this.form.get('selectedEndTime')?.value;
+    return  this.initTime(date, time);
+  }
+
+  private initTime(date: Date, time: string):string {
+    const result = new Date(date);
+    const hour = +time.substring(0, 2);
+    const minute = +time.substring(3);
+    result.setHours(hour);
+    result.setMinutes(minute);
+    result.setSeconds(0);
+    return result.toISOString();
+  }
+}
