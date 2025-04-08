78315273accf8a35c341e92d7080fda1b1d5f07a
Miklos Gabor <gmiki91@gmail.com>
2025-04-08 13:30:24 +0200

    remove ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, inject, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
-import { isPlatformBrowser } from '@angular/common';
 import { UserService } from './services/user.service';
 
 @Component({
@@ -15,16 +14,13 @@ export class AppComponent implements OnInit {
   title = 'Sign sign';
   userService = inject(UserService);
   loading = true;
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   async ngOnInit(): Promise<void> {
-    if (isPlatformBrowser(this.platformId)) {
       if (!!localStorage.getItem('authToken')) {
         await this.userService.initializeUser();
       } else {
         await this.userService.initializeGuest();
       }
       this.loading = false;
-    }
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,8 @@
-import { inject, Injectable, signal, Inject, PLATFORM_ID } from '@angular/core';
+import { inject, Injectable, signal} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import {Visitor} from '../models/user.model';
 import { MockUser } from '../utils/mock.factory';
-import { isPlatformBrowser } from '@angular/common';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
@@ -14,11 +13,8 @@ export class UserService {
   user = this._user.asReadonly();
   isAuthenticated$=new BehaviorSubject<boolean>(false);
 
-  constructor( @Inject(PLATFORM_ID) private platformId: Object){}
-
   initializeUser(): Promise<void> {
     return new Promise((resolve) => {
-      if (isPlatformBrowser(this.platformId)) {
         const token = localStorage.getItem('authToken');
         if (token) {
           this.http.get<Visitor>(`${this.url}/me`,{
@@ -42,16 +38,12 @@ export class UserService {
         } else {
           resolve(); // Resolve immediately if no token is found
         }
-      } else {
-        resolve(); // Resolve immediately for non-browser platforms
-      }
     }
   );
   }
 
   initializeGuest(): Promise<void> {
     return new Promise((resolve) => {
-      if (isPlatformBrowser(this.platformId)) {
         const uid = localStorage.getItem('uuid') || '0';
         this.http.get<Visitor>(`${this.url}/check/${uid}`).subscribe({
           next: (user) => {
@@ -65,9 +57,6 @@ export class UserService {
             resolve();
           },
         });
-      } else {
-        resolve(); // Resolve immediately for non-browser platforms
-      }
     });
   }
 
@@ -76,12 +65,7 @@ export class UserService {
   }
 
   setUser(user: Visitor): void {
-    if (user && isPlatformBrowser(this.platformId)) {
       this._user.set(user);
- 
-    } else {
-      console.log('Something went horrible wrong! Is this in the browser?',isPlatformBrowser(this.platformId));
-    }
   }
 
   logout(): void {
