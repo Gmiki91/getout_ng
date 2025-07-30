0cc86452ceae7eb9fc1aef10c6a80e60b7531eda
Miklos Gabor <gmiki91@gmail.com>
2025-07-30 18:20:50 +0200

    add skeleton loader

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import {  provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideNgxSkeletonLoader } from 'ngx-skeleton-loader';
+import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { credentialsInterceptor } from './interceptors/credential.interceptor';
 
@@ -13,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideHttpClient(withFetch(), withInterceptors([authInterceptor,credentialsInterceptor])), 
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideNgxSkeletonLoader()
   ]
 };
--- src/app/event-sidebar/event-sidebar.component.ts
+++ src/app/event-sidebar/event-sidebar.component.ts
@@ -5,20 +5,23 @@ import { MapService } from '../services/map.service';
 import { Event } from '../models/event.model';
 import { EventListComponent } from '../event-list/event-list.component';
 import { FilterComponent } from '../filter/filter.component';
+import { StateService } from '../services/state.service';
+import { NgxSkeletonLoaderComponent } from "ngx-skeleton-loader";
 
 @Component({
   selector: 'app-event-sidebar',
-  imports: [FilterComponent, MatLabel, EventListComponent, FilterComponent],
+  imports: [FilterComponent, MatLabel, EventListComponent, FilterComponent, NgxSkeletonLoaderComponent],
   templateUrl: './event-sidebar.component.html',
   styleUrl: './event-sidebar.component.scss',
 })
 export class EventSidebarComponent {
   private eventsService = inject(EventsService);
   private mapService = inject(MapService);
+  private stateService = inject(StateService);
   joinedEvents = this.eventsService.yourEvents;
   otherEvents = this.eventsService.otherEvents;
   selectedEvent = this.eventsService.selectedEvent;
-
+  showEvents = this.stateService.areEventsLoaded;
   onOpenDetails(event: Event): void {
     this.mapService.flyTo(event.latLng);
     this.mapService.highlightMarker(event.id);
