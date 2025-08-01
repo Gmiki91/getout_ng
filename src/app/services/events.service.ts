15b3853e3843249ebdeb9afae084a079d5e5eedf
Miklos Gabor <gmiki91@gmail.com>
2025-08-01 16:37:17 +0200

    update filter and sort handling

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -3,17 +3,18 @@ import { NewEventData, Event, LatLng, UpdateEventData } from '../models/event.mo
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { catchError, Observable, tap, throwError } from 'rxjs';
-import { calculateDistance } from '../utils/utils';
 import { UserService } from './user.service';
 import { MapService } from './map.service';
 import { StateService } from './state.service';
+import { filterAndSortEvents } from '../utils/event-processing.utils';
+import { calculateDistance } from '../utils/distance.utils';
 
 @Injectable({ providedIn: 'root' })
 export class EventsService {
   private url = environment.url + 'events';
   private bridgeUrl = environment.url + 'user-events';
   private _http = inject(HttpClient);
-  private _mapService = inject(MapService)
+  private _mapService = inject(MapService);
   private _userService = inject(UserService);
   private _stateService = inject(StateService);
 
@@ -22,12 +23,20 @@ export class EventsService {
   private _selectedEvent = signal<Event>({} as Event);
   private _currentPosition = signal<LatLng>({ lat: 0, lng: 0 });
   private _user = this._userService.user;
-  private _unfilteredOtherEvents: Event[] = [];
-  private _unfilteredYourEvents: Event[] = [];
   currentPosition = this._currentPosition.asReadonly();
-  yourEvents = this._yourEvents.asReadonly();
-  otherEvents = this._otherEvents.asReadonly();
   selectedEvent = this._selectedEvent.asReadonly();
+
+  otherEvents = filterAndSortEvents(
+    this._otherEvents,
+    this._stateService,
+    this._mapService
+  );
+
+  yourEvents = filterAndSortEvents(
+    this._yourEvents,
+    this._stateService,
+    this._mapService
+  );
 
   //save the sorted events for the events list, returns unsorted for the map markers
   getEvents(): Observable<{ joinedEvents: Event[]; otherEvents: Event[] }> {
@@ -47,16 +56,6 @@ export class EventsService {
       );
   }
 
-  setEvents(joinedEvents: Event[], otherEvents: Event[]): void {
-    const updatedOtherEvents = this.updateDistanceOfEvents(otherEvents);
-    const updatedYourEvents = this.updateDistanceOfEvents(joinedEvents);
-
-    this._unfilteredOtherEvents = updatedOtherEvents;
-    this._unfilteredYourEvents = updatedYourEvents;
-    this._otherEvents.set(updatedOtherEvents);
-    this._yourEvents.set(updatedYourEvents);
-  }
-
   addEvent(event: NewEventData): Observable<Event> {
     if (this._user().id) event = { ownerId: this._user().id, ...event };
     return this._http.post<Event>(`${this.bridgeUrl}`, event).pipe(
@@ -67,42 +66,15 @@ export class EventsService {
     );
   }
 
-  updateEvent(event:UpdateEventData):Observable<Event>{
-    return this._http.patch<Event>(`${this.url}/${event.id}`,event).pipe(tap(event=>this.updateList(event)));
+  updateEvent(event: UpdateEventData): Observable<Event> {
+    return this._http
+      .patch<Event>(`${this.url}/${event.id}`, event)
+      .pipe(tap((event) => this.updateList(event)));
   }
-
- private updateList(event: UpdateEventData): void {
-    const isUserJoined = this._yourEvents().some(yourEvent => yourEvent.id === event.id);
-    const eventsList = isUserJoined ? this._yourEvents() : this._otherEvents();
-    const eventToUpdate = eventsList.find(existingEvent => existingEvent.id === event.id);
-  
-    if (eventToUpdate) {
-      // Merge the event with the new data
-      const updatedEvent = { ...eventToUpdate, ...event };
-      // If a new location (latLng) is provided, calculate the distance
-      if (event.latLng) {
-        updatedEvent.distance = this.addDistance(event.latLng);
-        this._mapService.removeMarkerById(updatedEvent.id);
-        this._mapService.addMarker(updatedEvent);
-      }
-  
-      // Update the corresponding event list
-      if (isUserJoined) {
-        this._yourEvents.set(this._yourEvents().filter(yourEvent=>yourEvent.id !==event.id));
-        this._yourEvents.update((events) => [updatedEvent, ...events]);
-      } else {
-        this._otherEvents.set(this._otherEvents().filter(otherEvent=>otherEvent.id !==event.id));
-        this._otherEvents.update((events) => [updatedEvent, ...events]);
-      }
-    }
-  }
-  
 
   deleteEvent(eventId: string, ownerId: string): void {
     if (this._user().id === ownerId) {
       this._http.delete(`${this.bridgeUrl}/events/${eventId}`).subscribe(() => {
-        this._unfilteredYourEvents = this._unfilteredYourEvents.filter((e) => e.id !== eventId);
-        this._unfilteredOtherEvents = this._unfilteredOtherEvents.filter((e) => e.id !== eventId);
         this._otherEvents.update((events) =>
           events.filter((e) => e.id !== eventId)
         );
@@ -149,80 +121,74 @@ export class EventsService {
   selectEvent(event: Event): void {
     this._selectedEvent.set(event);
     this._mapService.removeTemporaryMarker();
-    this._mapService.setPosition(event.location,event.latLng);
+    this._mapService.setPosition(event.location, event.latLng);
     this._stateService.toggleEventDetails();
   }
 
   selectEventById(eventId: string): void {
     const events = this._yourEvents().concat(this._otherEvents());
-    const event = events.find(event => event.id === eventId);
-    if(event){
+    const event = events.find((event) => event.id === eventId);
+    if (event) {
       this.selectEvent(event);
-    }else{
-      console.log('event not found')
+    } else {
+      console.log('event not found');
     }
   }
 
   //for distance calculation and autofill proximity
-  setCurrentPosition(position:LatLng){
+  setCurrentPosition(position: LatLng) {
     this._currentPosition.set(position);
-  }
-
-  // FILTER METHODS
-  sortByDistance(ascending: boolean): void {
-    const funk = ascending
-      ? (a: Event, b: Event) => a.distance - b.distance
-      : (a: Event, b: Event) => b.distance - a.distance;
-    this._otherEvents.update((events) => {
-      return events.sort(funk);
-    });
-    this._yourEvents.update((events) => {
-      return events.sort(funk);
-    });
-  }
-
-  sortByTime(ascending: boolean): void {
-    const funk = ascending
-      ? (a: Event, b: Event) =>
-          new Date(a.time).getTime() - new Date(b.time).getTime()
-      : (a: Event, b: Event) =>
-          new Date(b.time).getTime() - new Date(a.time).getTime();
-    this._otherEvents.update((events) => {
-      return events.sort(funk);
-    });
-    this._yourEvents.update((events) => {
-      return events.sort(funk);
-    });
-  }
-
-  applyFilters(hide: boolean, distance: number): void {
-    this._otherEvents.set(
-      this._unfilteredOtherEvents.filter((event) => {
-        const passesFullFilter = !hide || (event.max == 0 || event.max > event.participants.length);
-        const passesDistanceFilter = event.distance <= distance * 1000;
-         if(!passesDistanceFilter || !passesFullFilter) this._mapService.removeMarkerById(event.id);
-         if(passesDistanceFilter && passesFullFilter) this._mapService.addMarker(event);
-        return passesFullFilter && passesDistanceFilter;
-      })
-    );
-
-    this._yourEvents.set(
-      this._unfilteredYourEvents.filter(event=>{
-        const passesDistanceFilter = event.distance <= distance * 1000;
-        if(!passesDistanceFilter) this._mapService.removeMarkerById(event.id);
-        else this._mapService.addMarker(event);
-        return passesDistanceFilter;
-      })
-    );
   }
 
   isUserJoined(id: string): boolean {
     return this.yourEvents().some((e) => e.id == id);
   }
 
-  updateDistances():void{
+  updateDistances(): void {
     this.updateDistanceOfEvents(this._otherEvents());
     this.updateDistanceOfEvents(this._yourEvents());
+  }
+
+  private setEvents(joinedEvents: Event[], otherEvents: Event[]): void {
+    const updatedOtherEvents = this.updateDistanceOfEvents(otherEvents);
+    const updatedYourEvents = this.updateDistanceOfEvents(joinedEvents);
+
+    this._otherEvents.set(updatedOtherEvents);
+    this._yourEvents.set(updatedYourEvents);
+  }
+
+  private updateList(event: UpdateEventData): void {
+    const isUserJoined = this._yourEvents().some(
+      (yourEvent) => yourEvent.id === event.id
+    );
+    const eventsList = isUserJoined ? this._yourEvents() : this._otherEvents();
+    const eventToUpdate = eventsList.find(
+      (existingEvent) => existingEvent.id === event.id
+    );
+
+    if (eventToUpdate) {
+      // Merge the event with the new data
+      const updatedEvent = { ...eventToUpdate, ...event };
+      // If a new location (latLng) is provided, calculate the distance
+      if (event.latLng) {
+        updatedEvent.distance = this.addDistance(event.latLng);
+        this._mapService.removeMarkerById(updatedEvent.id);
+        this._mapService.addMarker(updatedEvent);
+      }
+
+      // Update the corresponding event list
+      if (isUserJoined) {
+        this._yourEvents.set(
+          this._yourEvents().filter((yourEvent) => yourEvent.id !== event.id)
+        );
+        this._yourEvents.update((events) => [updatedEvent, ...events]);
+      } else {
+        this._otherEvents.set(
+          this._otherEvents().filter((otherEvent) => otherEvent.id !== event.id)
+        );
+        this._otherEvents.update((events) => [updatedEvent, ...events]);
+      }
+    }
   }
 
   private updateDistanceOfEvents(events: Event[]): Event[] {
