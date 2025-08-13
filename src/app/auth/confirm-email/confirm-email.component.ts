b47cdcb16eb2d929f49fc20d0d6a805f0185b537
Miklos Gabor <gmiki91@gmail.com>
2025-08-13 10:34:00 +0200

    update email confirm component

--- src/app/auth/confirm-email/confirm-email.component.ts
+++ src/app/auth/confirm-email/confirm-email.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute,Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -11,14 +11,31 @@ import { AuthService } from '../../services/auth.service';
 export class ConfirmEmailComponent implements OnInit {
   authService = inject(AuthService);
   route = inject(ActivatedRoute);
-  message = '';
+  router = inject(Router);
+  message = 'Invalid or expired token.';
+   countdown = 5;
+   isTokenValid = false;
 ngOnInit() {
   const token = this.route.snapshot.queryParamMap.get('token');
   if (token) {
     this.authService.confirmEmail(token).subscribe({
-      next: () => this.message = "Email confirmed!",
-      error: () => this.message = "Invalid or expired token."
+      next: () => {
+        this.message = "Email confirmed!";
+        this.startCountdown();},
+      error: () => this.message = "Invalid or expired token!",
+
     });
   }
 }
+  startCountdown() {
+    this.isTokenValid = true;
+    const interval = setInterval(() => {
+      this.countdown--;
+      if (this.countdown <= 0) {
+        clearInterval(interval);
+        this.isTokenValid = false;
+        this.router.navigate(['/']);
+      }
+    }, 1000);
+  }
 }
